63223d6148b6753e7fff8fc95b9e971a9e63efb5
Joe Paul Sajot <joepaulsajot@gmail.com>
2023-09-07 21:04:04 +0800

    fix rounded

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com", "_blank")
+            window.open("https://github.com/aOzSparKz/Jps-QuantumAI", "_blank")
           }
           className='black_btn'
         >
